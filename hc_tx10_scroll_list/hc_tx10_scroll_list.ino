b0d243ec59e2250b00509d9f57290ece2f085ea4
Kevin <k.johnson.automation@gmail.com>
2020-11-21 17:06:46 -0500

    added lcd screen support

--- hc_tx10_scroll_list/hc_tx10_scroll_list.ino
+++ hc_tx10_scroll_list/hc_tx10_scroll_list.ino
@@ -22,6 +22,9 @@ bool DEBUG = false;
 //Color Defines:
 #define WHITE 0x7
 
+String ROUTINES[9] = {"On/Off", "Fade", "Twinkle", "Flash", "Dual", "Chase", "Steady", "Combo", "Auto"};
+int count_index = 0;
+
 /* Sequences for Light Control 
  *  On Off  Sequence = 100011101110111010001000100011101110100010001000111011101110100011
  *  Fade    Sequence = 100011101110111010001110111011101110100010001000111010001000100011
@@ -50,9 +53,8 @@ void setup() {
   // set up the LCD's number of columns and rows: 
   lcd.begin(16, 2);
   lcd.setBacklight(WHITE);
-
   lcd.print("MERRY CHRISTMAS!");
-  
+//  int (*Function_Ptr)(9);
 }
 
 
@@ -125,6 +127,9 @@ void send_pattern(char *pattern) {
 // Toggles lights on_off
 void on_off_sequence() {
   //100011101110111010001000100011101110100010001000111011101110100011
+  if (DEBUG) {
+    Serial.println("Sending On Off");
+  }
   char sequence[] = "100011101110111010001000100011101110100010001000111011101110100011";
   send_pattern(sequence);
 }
@@ -177,13 +182,48 @@ void auto_sequence() {
   send_pattern(sequence);
 }
 
+int up_list_num(int current_val) {
+  // Moves up the scroll list
+  if (DEBUG) {
+    Serial.println(current_val);
+  }
+  if (current_val == 0) {
+    if (DEBUG) {
+        Serial.println(8);
+    }
+     return 8;
+  }
+  else {
+    if (DEBUG) {
+      Serial.println(--current_val);
+    }
+    return --current_val;
+  }
+}
+
+int down_list_num(int current_val) {
+  // Moves down the scroll list
+  if (DEBUG) {
+    Serial.println(current_val);
+  }
+  if (current_val == 8) {
+      if (DEBUG) {
+        Serial.println(0);
+      }
+    return 0;
+  }
+  else {
+    if (DEBUG) {
+      Serial.println(++current_val);
+    }
+    return ++current_val;
+  }
+}
+
 // the loop routine runs over and over again forever:
 void loop() {
-  //Clears screen
-  lcd.setCursor(0, 1);
-  if (DEBUG) {
-    Serial.println("Looping");
-  }
+  void (*Function_Ptr[9])() = {on_off_sequence, fade_sequence, twinkle_sequence, flash_sequence, dual_color_sequence, chasing_sequence, steady_sequence, combo_sequence, auto_sequence};
+
   uint8_t buttons = lcd.readButtons();
 
   if (buttons) {
@@ -194,38 +234,47 @@ void loop() {
     if (buttons & BUTTON_LEFT){
       lcd.setCursor(0,1);
       lcd.print("On/Off");
-      for (int counter=0;counter<100;counter++) {
+      for (int counter=0;counter<20;counter++) {
         on_off_sequence();
-      }
-    }
-    if (buttons & BUTTON_UP) {
-      lcd.setCursor(0,1);
-      lcd.print("Twinkle");
-      for (int counter=0;counter<100;counter++) {
-        twinkle_sequence();
-      }
-    }
-    if (buttons & BUTTON_DOWN) {
-      lcd.setCursor(0,1);
-      lcd.print("Steady On");
-      for (int counter=0;counter<100;counter++) {
-        steady_sequence();
       }
     }
     if (buttons & BUTTON_RIGHT) {
       lcd.setCursor(0,1);
-      lcd.print("Chasing!");
-      for (int counter=0;counter<100;counter++) {
-        chasing_sequence();
+      lcd.print("Auto");
+      for (int counter=0;counter<20;counter++) {
+        auto_sequence();
       }
     }
+    //Scroll up through the list
+    if (buttons & BUTTON_UP) {
+      count_index = up_list_num(count_index);
+      lcd.setCursor(0,1);
+      lcd.print(ROUTINES[count_index]);
+    }
+
+    //Scroll down through the list
+    if (buttons & BUTTON_DOWN) {
+      count_index = down_list_num(count_index);
+      lcd.setCursor(0,1);
+      lcd.print(ROUTINES[count_index]);
+    }
+
     if (buttons & BUTTON_SELECT) {
+      if (DEBUG) {
+        Serial.println("sending: " + ROUTINES[count_index]);
+      }
+      lcd.setCursor(0,1);
+      lcd.print("Sending " + ROUTINES[count_index]);
+      for (int counter=0;counter<20;counter++) {
+        Function_Ptr[count_index]();
+      }
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("MERRY CHRISTMAS!");
       lcd.setCursor(0,1);
       lcd.print("HO HO HO!");
     }
   }
-//  for (int counter=0;counter<100;counter++){
-//    on_off_sequence();
-//    delayMicroseconds(15000);
+  //Adding button repetion
   delay(50);
 }
